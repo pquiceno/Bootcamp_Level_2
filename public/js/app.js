626bdf95221ee9aa5ad0030077c3032fb433114d
Pablo Quiceno <jpquiceno@kometsales.com>
2017-03-29 22:15:50 -0500

    Se termina el segundo bootcamp

---
+++ public/js/app.js
@@ -0,0 +1,75 @@
+(function (){
+
+	var STORAGE_KEY = 'usuarios'
+
+	Vue.component('modal', {
+	  template: '#modal-template'
+	});
+
+	var app = new Vue({
+	  el: '#app',
+	  data: {
+	    showModal: false,
+	    items: JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+	  }
+	});
+
+	var usersStorage = {
+	  fetch: function () {
+	    var usuarios = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+	    usuarios.forEach(function (todo, index) {
+	      todo.id = index
+	    });
+	    return usuarios;
+	  },
+	  save: function (usuarios) {
+	    localStorage.setItem(STORAGE_KEY, JSON.stringify(usuarios));
+	  }
+	}
+
+	var actions = { 
+		addUser : function () {
+			var nameInput = document.getElementById("name").value;
+			var lastnameInput = document.getElementById("lastname").value;
+			if(nameInput && lastnameInput){
+				var users = usersStorage.fetch();
+				users.push({
+					name: nameInput,
+					lastname: lastnameInput
+				});
+				usersStorage.save(users);
+				app.items = usersStorage.fetch();
+			}
+	    },
+	    deleteUsers : function () {
+	    	localStorage.clear();
+	    	app.items = [];
+	    }
+	}
+
+    var btnAdd = document.getElementById("btnAdd");
+    btnAdd.addEventListener("click", actions.addUser);
+
+    var btnClear = document.getElementById("btnClear");
+    btnClear.addEventListener("click", actions.deleteUsers);
+
+
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	ctx.fillRect(25,25,100,100);
+    ctx.clearRect(45,45,60,60);
+    ctx.strokeRect(50,50,50,50);
+
+	ctx.beginPath();
+    ctx.moveTo(150,50);
+    ctx.lineTo(175,75);
+    ctx.lineTo(175,25);
+    ctx.fillStyle="#0B6121";
+    ctx.fill();
+
+	ctx.beginPath();
+    ctx.arc(250,70,50,0,(Math.PI/180)*360,true);
+	ctx.fillStyle="#013ADF";
+	ctx.fill();
+})();
+
---
+++ readme.md
@@ -0,0 +1,24 @@
+<img src="avatar-devlabs.jpg" alt="Smiley face" height="350" width="500" align="center">
+
+#Node.js + Express + Handlebars
+
+#OOP y herencia en JavaScript
+
+En este tema nos centraremos en aprender cómo JavaScript aborda la programación orientada a objetos.
+
+##Lectura:
+1. Un poco de prototype en EMACScript 5.1 http://yehudakatz.com/2011/08/12/understanding-prototypes-in-javascript/
+2. ECMAScript 6 y nuevas funcionalidades: http://es6-features.org/.
+3. Understanding ECMAScript6
+4. Modulos en ES6(EMACScript6) ES6 Modules
+
+##Recomendados
+1. AMD, CommonJS, and ES6 Modules Writing Modular JavaScript: https://github.com/lukehoban/es6features
+
+## How To
+
+Logra correr la linea base solo en 3 pasos y comenzar el nivel!
+
+1. Asegurarse de tener instalado [NodeJS](https://nodejs.org/) y [npm](https://www.npmjs.com/).
+2. Ingresar en la terminal : npm install.
+3. Iniciar proyecto: npm start.
